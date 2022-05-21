0a9c644d6abddf5c01eee19fda1352a42eeefc33
alexandre <AlexandreOliveira12>
2022-05-21 01:08:41 -0300

    atualização 2.0

---
+++ script.js
@@ -0,0 +1,84 @@
+//ScrollReveal
+window.sr = ScrollReveal({ reset: true});
+
+sr.reveal('.BoasVindas', {duration:1500});
+
+sr.reveal('.BoasVindas2', {duration:1500});
+
+sr.reveal('Memes', {duration:1700});
+
+sr.reveal('.infojs', {duration:1500});
+
+sr.reveal('.infojs2', { duration:1500});
+
+sr.reveal('.infopy', {duration:1500});
+
+sr.reveal('.infopy2', {duration:1500});
+
+sr.reveal('.infoso', {duration:1500});
+
+sr.reveal('.LiWi', {duration:1500});
+
+sr.reveal('.IntroLi', {duration:1500});
+
+sr.reveal('.VanLi', {duration:1500});
+
+sr.reveal('.IntroWi', {duration:1500});
+
+sr.reveal('.VanWi', {duration:1500});
+
+sr.reveal('.Happy', {duration:1500});
+
+sr.reveal('.Criador', {duration:1500});
+
+sr.reveal('.contato', {duration:1500});
+
+sr.reveal('.Lingua', {duration:1500});
+
+sr.reveal('.SO', {duration:1500});
+
+sr.reveal('.CF', {duration:1500});
+
+sr.reveal('.img1', {duration:1700}); //Hello word
+
+sr.reveal('.img2', {duration:1700});//meme1
+
+sr.reveal('.img3', {duration:1700});//meme2
+
+sr.reveal('.img4', {duration:1700});//meme3
+
+sr.reveal('.img5', {duration:1700});//meme4
+
+sr.reveal('.img6', {duration:1700});//meme5
+
+sr.reveal('.img7', {duration:1700});//meme6
+
+sr.reveal('.img8', {duration:1700});//Hello world js
+
+sr.reveal('.img9', {duration:1700});//variavel js
+
+sr.reveal('.img10', {duration:1700});//hello world python
+
+sr.reveal('.img11', {duration:1700});//variavel python
+
+sr.reveal('.img12', {duration:1700});//Linux x Windows
+
+sr.reveal('.insta', {duration:1700});//insta
+
+sr.reveal('.dc', {duration:1700});//discord
+
+sr.reveal('.git', {duration:1700});//GitHub
+
+sr.reveal('.devicon-linux-plain', {duration:1700});//Logo linux
+
+sr.reveal('.html', {duration:1700});//language html
+
+sr.reveal('.css', {duration:1700});//language css
+
+sr.reveal('.JavaScript', {duration:1700});//language JavaScript
+
+sr.reveal('.div1', {duration:1000});//Divisão 1
+
+sr.reveal('.div2', {duration:1900});//Divisão 2
+
+sr.reveal('.doc', {duration:1700});//codigo fonte .doc
